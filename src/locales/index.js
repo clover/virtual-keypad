e110fb9f41d7ba28f275a1911d01e1ba8afdd29e
Jason Whitted <jason.whitted@clover.com>
2020-09-22 12:25:54 -0600

    Add de-DE, fr-CA, nl-NL localization

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,3 +1,11 @@
 import en from './en.json';
+import deDE from './de-DE.json';
+import frCA from './fr-CA.json';
+import nlNL from './nl-NL.json';
 
-export default { en };
+export default {
+  en,
+  'de-DE': deDE,
+  'fr-CA': frCA,
+  'nl-NL': nlNL,
+};
