f21562666673fa60f942390545f443cb9153bdc4
Jason Whitted <jason.whitted@clover.com>
2022-01-21 13:35:07 -0700

    Update API calls to pass access_token via header instead of querystring

--- src/common/api.js
+++ src/common/api.js
@@ -2,47 +2,37 @@ import { PERMISSION } from './constants';
 import retryFetch from './retryFetch';
 
 export const deviceApps = ({ cloverDomain, merchantId, deviceId, accessToken }) =>
-  retryFetch(
-    new URL(
-      `/v2/merchant/${merchantId}/device/${deviceId}/current_apps?access_token=${accessToken}`,
-      cloverDomain
-    ).toString(),
-    {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-      },
-    }
-  ).then(r => r.json());
-
-export const devices = ({ cloverDomain, merchantId, accessToken }) =>
-  retryFetch(new URL(`/v3/merchants/${merchantId}/devices?access_token=${accessToken}`, cloverDomain).toString(), {
+  retryFetch(new URL(`/v2/merchant/${merchantId}/device/${deviceId}/current_apps`, cloverDomain).toString(), {
     method: 'GET',
     headers: {
       accept: 'application/json',
       'content-type': 'application/json',
+      authorization: `Bearer ${accessToken}`,
+    },
+  }).then(r => r.json());
+
+export const devices = ({ cloverDomain, merchantId, accessToken }) =>
+  retryFetch(new URL(`/v3/merchants/${merchantId}/devices`, cloverDomain).toString(), {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      authorization: `Bearer ${accessToken}`,
     },
   }).then(r => r.json());
 
 export const employee = ({ cloverDomain, merchantId, employeeId, accessToken }) =>
-  retryFetch(
-    new URL(
-      `/v3/merchants/${merchantId}/employees/${employeeId}?access_token=${accessToken}&expand=roles`,
-      cloverDomain
-    ).toString(),
-    {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-      },
-    }
-  ).then(r => r.json());
+  retryFetch(new URL(`/v3/merchants/${merchantId}/employees/${employeeId}?expand=roles`, cloverDomain).toString(), {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      authorization: `Bearer ${accessToken}`,
+    },
+  }).then(r => r.json());
 
 export const permissionSets = ({ cloverDomain, merchantId, accessToken, filterNames = Object.values(PERMISSION) }) => {
   const url = new URL(`/v3/merchants/${merchantId}/permission_sets`, cloverDomain);
-  url.searchParams.set('access_token', accessToken);
   url.searchParams.set('expand', 'roles');
   url.searchParams.set('limit', 1000);
   if (filterNames?.length) {
@@ -53,6 +43,7 @@ export const permissionSets = ({ cloverDomain, merchantId, accessToken, filterNa
     headers: {
       accept: 'application/json',
       'content-type': 'application/json',
+      authorization: `Bearer ${accessToken}`,
     },
   }).then(r => r.json());
 };
