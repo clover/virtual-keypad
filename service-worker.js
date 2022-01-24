85c768eb615b28b39007159412127bfb0665935f
Jason Whitted <jason.whitted@clover.com>
2022-01-24 14:58:49 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/virtual-keypad/precache-manifest.b486b8416aa3a3c7bfa5d341711f1c43.js"
+  "/virtual-keypad/precache-manifest.b6491abef8b0bce038b492220a841d32.js"
 );
 
 self.addEventListener('message', (event) => {
